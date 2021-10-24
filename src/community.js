30bc69e3c90516f876ea2f92a02a3a41f040e5f7
awadalla <awadalla@cs.washington.edu>
2021-10-24 10:34:46 -0700

    added sample text

--- src/community.js
+++ src/community.js
@@ -1,15 +1,15 @@
-import React, {useState, useEffect} from 'react';
-import {Card, Button, InputGroup, FormControl} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import CardHeader from 'react-bootstrap/esm/CardHeader';
-import thumbs from './thumb.png';
-import redthumbs from './redThumb.png';
-import bookmarks from './bookmark.png';
-import colorBookmark from './colorBookmark.png';
-import 'bootstrap/dist/css/bootstrap.css';
-import voice from './voice.png'
-import CommentList from './CommentList';
-import CommentForm from './CommentForm';
+import React, { useState, useEffect } from "react";
+import { Card, Button, InputGroup, FormControl } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CardHeader from "react-bootstrap/esm/CardHeader";
+import thumbs from "./thumb.png";
+import redthumbs from "./redThumb.png";
+import bookmarks from "./bookmark.png";
+import colorBookmark from "./colorBookmark.png";
+import "bootstrap/dist/css/bootstrap.css";
+import voice from "./voice.png";
+import CommentList from "./CommentList";
+import CommentForm from "./CommentForm";
 
 export function Community() {
   const [imgSource, changeImage] = useState(thumbs);
@@ -34,7 +34,7 @@ export function Community() {
   useEffect(() => {
     setLoading(true);
 
-    fetch('http://localhost:1234')
+    fetch("http://localhost:1234")
       .then((res) => res.json())
       .then((res) => {
         setComments(res);
@@ -54,76 +54,77 @@ export function Community() {
     <>
       <div
         style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
         <h2 style={{}}>Community Feed</h2>
         <Card
-
           style={{
-            width: '45%',
-            display: 'flex',
+            width: "45%",
+            display: "flex",
             borderRadius: 25,
-            borderStyle: 'none',
-            backgroundColor: '#CCFFD1',
+            borderStyle: "none",
+            backgroundColor: "#CCFFD1",
             marginBottom: 15,
-            justifyContent:'center'
+            justifyContent: "center",
           }}
         >
           <Card.Header
             style={{
-              backgroundColor: '#5fc884',
-              width: '100%',
-              display: 'flex',
+              backgroundColor: "#5fc884",
+              width: "100%",
+              display: "flex",
               borderRadius: 25,
               borderBottomLeftRadius: 0,
               borderBottomRightRadius: 0,
-              justifyContent:'center'
+              justifyContent: "center",
             }}
           >
-            Anonymous Hippopotamus{' '}
+            Anonymous Hippopotamus{" "}
           </Card.Header>
           <Card.Body>
             <Card.Title>Word Content</Card.Title>
             <Card.Text>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Mental illness affects our work. It is associated, for instance,
+              with decreased productivity, mood irritability leading to
+              increased interpersonal conflict, increased absenteeism, and
+              short-term and long-term disability. Thus, creating a safe and
+              healthy workplace environment is essential. Providing resources to
+              a colleague who might suffer from a mental health problem or
+              guiding him or her to the right direction can be very helpful.
+              Encouraging people to seek professional care and providing
+              support, understanding and acceptance are essential.
             </Card.Text>
           </Card.Body>
           <Card.Body
             style={{
-              backgroundColor: '#5fc884',
+              backgroundColor: "#5fc884",
               borderBottomLeftRadius: 25,
               borderBottomRightRadius: 25,
             }}
           >
             <Card.Body
               style={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'center',
-                width: '10%',
-                padding:0
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                width: "10%",
+                padding: 0,
               }}
             >
               <Card.Body
                 style={{
-                  justifyContent: 'center',
-                  display: 'flex',
+                  justifyContent: "center",
+                  display: "flex",
                 }}
               >
                 <div
                   style={{
-                    flexDirection: 'column',
-                    display: 'flex',
-                    justifyContent: 'center',
+                    flexDirection: "column",
+                    display: "flex",
+                    justifyContent: "center",
                     paddingLeft: 5,
                   }}
                 >
@@ -133,16 +134,16 @@ export function Community() {
               </Card.Body>
               <Card.Body
                 style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  justifyContent: 'center',
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "center",
                 }}
               >
                 <div
                   style={{
-                    flexDirection: 'column',
-                    display: 'flex',
-                    justifyContent: 'center',
+                    flexDirection: "column",
+                    display: "flex",
+                    justifyContent: "center",
                   }}
                 >
                   <img src={imgSource2} onClick={changeBookmarkColor} />
@@ -153,19 +154,27 @@ export function Community() {
 
             <Card.Body
               style={{
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
                 paddingTop: 0,
-                justifyContent: 'center', paddingLeft: 8
+                justifyContent: "center",
+                paddingLeft: 8,
               }}
             >
-    
-
               <div className="col-4  pt-3 border-right">
                 <CommentForm addComment={addComment} />
               </div>
-              <div className="col-8  pt-3 bg-white" style={{display:'flex',justifyContent: 'center', width: '50%',borderRadius:15, marginLeft:8}}>
+              <div
+                className="col-8  pt-3 bg-white"
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  width: "50%",
+                  borderRadius: 15,
+                  marginLeft: 8,
+                }}
+              >
                 <CommentList loading={loading} comments={comments} />
               </div>
             </Card.Body>
@@ -173,64 +182,67 @@ export function Community() {
         </Card>
         <Card
           style={{
-            width: '45%',
-            display: 'flex',
+            width: "45%",
+            display: "flex",
             borderRadius: 25,
-            borderStyle: 'none',
-            backgroundColor: '#f5e8cd',
+            borderStyle: "none",
+            backgroundColor: "#f5e8cd",
             marginBottom: 15,
-            justifyContent:'center'
+            justifyContent: "center",
           }}
         >
           <Card.Header
             style={{
-              backgroundColor: '#E5AF42',
-              width: '100%',
-              display: 'flex',
+              backgroundColor: "#E5AF42",
+              width: "100%",
+              display: "flex",
               borderRadius: 25,
               borderBottomLeftRadius: 0,
               borderBottomRightRadius: 0,
-              justifyContent:'center'
+              justifyContent: "center",
             }}
           >
-            Anonymous Octopus{' '}
+            Anonymous Octopus{" "}
           </Card.Header>
           <Card.Body>
             <Card.Title>Word Content</Card.Title>
             <Card.Text>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. 
+              If you know someone who might be struggling with a mental health
+              problem, you can approach the person nicely and ask how they are
+              doing and listen to them. Listening can be so helpful. It’s
+              important to show empathy, by putting yourself in their shoes,
+              showing them you care and appreciate they must be going through a
+              difficult time. Depending on what the person discloses, you can
+              ask them if you could help them in any way.
             </Card.Text>
           </Card.Body>
           <Card.Body
             style={{
-              backgroundColor: '#E5AF42',
+              backgroundColor: "#E5AF42",
               borderBottomLeftRadius: 25,
               borderBottomRightRadius: 25,
             }}
           >
             <Card.Body
               style={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'center',
-                width: '10%',
-                padding:0
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                width: "10%",
+                padding: 0,
               }}
             >
               <Card.Body
                 style={{
-                  justifyContent: 'center',
-                  display: 'flex',
+                  justifyContent: "center",
+                  display: "flex",
                 }}
               >
                 <div
                   style={{
-                    flexDirection: 'column',
-                    display: 'flex',
-                    justifyContent: 'center',
+                    flexDirection: "column",
+                    display: "flex",
+                    justifyContent: "center",
                     paddingLeft: 5,
                   }}
                 >
@@ -240,16 +252,16 @@ export function Community() {
               </Card.Body>
               <Card.Body
                 style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  justifyContent: 'center',
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "center",
                 }}
               >
                 <div
                   style={{
-                    flexDirection: 'column',
-                    display: 'flex',
-                    justifyContent: 'center',
+                    flexDirection: "column",
+                    display: "flex",
+                    justifyContent: "center",
                   }}
                 >
                   <img src={imgSource2} onClick={changeBookmarkColor} />
@@ -260,19 +272,27 @@ export function Community() {
 
             <Card.Body
               style={{
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
                 paddingTop: 0,
-                justifyContent: 'center', paddingLeft: 8
+                justifyContent: "center",
+                paddingLeft: 8,
               }}
             >
-    
-
               <div className="col-4  pt-3 border-right">
                 <CommentForm addComment={addComment} />
               </div>
-              <div className="col-8  pt-3 bg-white" style={{display:'flex',justifyContent: 'center', width: '50%',borderRadius:15, marginLeft:8}}>
+              <div
+                className="col-8  pt-3 bg-white"
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  width: "50%",
+                  borderRadius: 15,
+                  marginLeft: 8,
+                }}
+              >
                 <CommentList loading={loading} comments={comments} />
               </div>
             </Card.Body>
@@ -280,62 +300,62 @@ export function Community() {
         </Card>
         <Card
           style={{
-            width: '45%',
-            display: 'flex',
+            width: "45%",
+            display: "flex",
             borderRadius: 25,
-            borderStyle: 'none',
-            backgroundColor: '#dfd6ee',
+            borderStyle: "none",
+            backgroundColor: "#dfd6ee",
             marginBottom: 15,
-            justifyContent:'center'
+            justifyContent: "center",
           }}
         >
           <Card.Header
             style={{
-              backgroundColor: '#8E6FCF',
-              width: '100%',
-              display: 'flex',
+              backgroundColor: "#8E6FCF",
+              width: "100%",
+              display: "flex",
               borderRadius: 25,
               borderBottomLeftRadius: 0,
               borderBottomRightRadius: 0,
-              justifyContent:'center'
+              justifyContent: "center",
             }}
           >
-            Anonymous Possum{' '}
+            Anonymous Possum{" "}
           </Card.Header>
           <Card.Body>
             <Card.Title>Word Content</Card.Title>
             <Card.Text>
-            My mental state for now, and how I make it. #MentalHealth
+              My mental state for now, and how I make it. #MentalHealth
             </Card.Text>
-            <Card.Img src={voice} style={{width:'50%'}}></Card.Img>
+            <Card.Img src={voice} style={{ width: "50%" }}></Card.Img>
           </Card.Body>
           <Card.Body
             style={{
-              backgroundColor: '#8E6FCF',
+              backgroundColor: "#8E6FCF",
               borderBottomLeftRadius: 25,
               borderBottomRightRadius: 25,
             }}
           >
             <Card.Body
               style={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'center',
-                width: '10%',
-                padding:0
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                width: "10%",
+                padding: 0,
               }}
             >
               <Card.Body
                 style={{
-                  justifyContent: 'center',
-                  display: 'flex',
+                  justifyContent: "center",
+                  display: "flex",
                 }}
               >
                 <div
                   style={{
-                    flexDirection: 'column',
-                    display: 'flex',
-                    justifyContent: 'center',
+                    flexDirection: "column",
+                    display: "flex",
+                    justifyContent: "center",
                     paddingLeft: 5,
                   }}
                 >
@@ -345,16 +365,16 @@ export function Community() {
               </Card.Body>
               <Card.Body
                 style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  justifyContent: 'center',
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "center",
                 }}
               >
                 <div
                   style={{
-                    flexDirection: 'column',
-                    display: 'flex',
-                    justifyContent: 'center',
+                    flexDirection: "column",
+                    display: "flex",
+                    justifyContent: "center",
                   }}
                 >
                   <img src={imgSource2} onClick={changeBookmarkColor} />
@@ -365,19 +385,27 @@ export function Community() {
 
             <Card.Body
               style={{
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
                 paddingTop: 0,
-                justifyContent: 'center', paddingLeft: 8
+                justifyContent: "center",
+                paddingLeft: 8,
               }}
             >
-    
-
               <div className="col-4  pt-3 border-right">
                 <CommentForm addComment={addComment} />
               </div>
-              <div className="col-8  pt-3 bg-white" style={{display:'flex',justifyContent: 'center', width: '50%',borderRadius:15, marginLeft:8}}>
+              <div
+                className="col-8  pt-3 bg-white"
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  width: "50%",
+                  borderRadius: 15,
+                  marginLeft: 8,
+                }}
+              >
                 <CommentList loading={loading} comments={comments} />
               </div>
             </Card.Body>
